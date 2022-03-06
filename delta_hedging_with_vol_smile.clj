530956a2c58a7ea539655972cda66e08a4a535f1
Wang Xizhuo <71929217+WangXizhuo@users.noreply.github.com>
2022-03-06 14:27:24 +0800

    Update delta_hedging_with_vol_smile.clj

--- delta_hedging_with_vol_smile.clj
+++ delta_hedging_with_vol_smile.clj
@@ -188,7 +188,6 @@
 
 (defn -main
   [& args]
-;;****************************************************************************************************
   (reset! data-set (add-aprc (change-format-stock (read-csv-row "./resources/stock2020.csv"))))
 
   (def opts (atom {}))
@@ -228,52 +227,6 @@
     (update-eval-report (get-date))
     (next-date))
     (eval-report -1)
-;;****************************************************************************************************
-
-  ;; (reset! data-set (add-aprc (read-csv-row "./resources/CRSP-extract.csv")))
-  ;; ;(reset! data-set (add-aprc (read-csv-row "./resources/data-CRSP-lohi-extract-1000.csv")))
-  ;; (init-portfolio "1986-01-09" 2000)
-  ;; (let [tot-val (get-in (deref portfolio) [:cash :tot-val])]
-  ;;   (println tot-val)
-  ;; )
-
-  ;; (order "AAPL" 10)
-  ;; ;(order "OMFGA" 10)
-  ;; (next-date)
-
-  ;; (update-eval-report (get-date))
-  ;; (next-date)
-  ;; (update-eval-report (get-date))
-
-  ;; (println (portfolio-daily-ret))
-  ;; (next-date)
-  ;; (next-date)
-  ;; (next-date)
-  ;; (next-date)
-  ;; (next-date)
-
-  ;; (pprint/print-table (deref order-record))
-  ;; (view-portfolio)
-  ;; (view-portfolio-record -1)
-  ;; (eval-report -1)
-
-  ;; ;; (println (sd-last-n-days "OMFGA" 10))
-  ;; ;; (let [prev-close (Double/parseDouble (get (first (get-prev-n-days :PRC 1 "OMFGA")) :PRC))]
-  ;; ;;   (println (parabolic-SAR "OMFGA" "non-lazy" 0.2 prev-close))
-  ;; ;;   )
-
-  ;; ;; (let [low-price (Double/parseDouble (get-by-key "OMFGA" :BIDLO "non-lazy"))
-  ;; ;;       high-price (Double/parseDouble (get-by-key "OMFGA" :ASKHI "non-lazy"))
-  ;; ;;       prev-atr (- high-price low-price)]
-  ;; ;;   (println (keltner-channel "OMFGA" "non-lazy" 10 prev-atr))
-  ;; ;;   )
-
-  ;; ;; (println (force-index "OMFGA" "non-lazy" 20))
-;;****************************************************************************************************
-
-
-  ;; (end-order)
-
-)
+  (end-order))
 
 
