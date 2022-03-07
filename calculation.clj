58e2bef70e3a88157c19f03c1f76dc0a1310fefd
Wang Xizhuo <71929217+WangXizhuo@users.noreply.github.com>
2022-03-07 23:06:56 +0800

    Update calculation.clj

--- calculation.clj
+++ calculation.clj
@@ -33,12 +33,43 @@
                                                             :impl_volatility (parse-double (get first-line :impl_volatility))}))
                     (recur next-remaining result-set))))))
 
+
+
+
+(defn option-BS-delta
+  "Calculate the Black Scholes option delta."
+  [opts]
+  (let [weights (map :weight opts)
+        deltas (map :delta opts)
+        vegas (map :vega opts)
+        length (count weights)]
+        (loop [counter 0 opt-delta 0]
+          (if (= counter (- length 1))
+          (* opt-delta 100)
+          (let [BS_delta (nth deltas counter)]
+            (recur (inc counter) (+ opt-delta (* (nth weights counter) BS_delta))))))))
+
+
 (defn adj_term
   "Do the regression and return adjustment term dvol/dK."
   [vol_K]
   (let [vol (into [] (map :impl_volatility vol_K))
         K (into [] (map :strike vol_K))]
         (last (:coefs (linear-model vol K)))))
+
+(defn option-adj-delta
+  "Calculate the adjusted option delta."
+  [opts dvol-dK]
+  (let [weights (map :weight opts)
+        deltas (map :delta opts)
+        vegas (map :vega opts)
+        length (count weights)]
+        (loop [counter 0 opt-delta 0]
+          (if (= counter (- length 1))
+          (* opt-delta 100)
+          (let [adj_delta (+ (nth deltas counter) (* (nth vegas counter) dvol-dK))]
+            (recur (inc counter) (+ opt-delta (* (nth weights counter) adj_delta))))))))
+
 
 (defn add-weight
   "Calculate the weight of each option according to their trading volume."
@@ -52,17 +83,5 @@
                 next-remaining (rest remaining)]
                 (recur next-remaining (conj added-weight-opts (assoc first-line :weight (/ (:volume first-line) tot-volume)))))))))
 
-(defn option-delta
-  "Calculate the adjusted option delta."
-  [opts dvol-dK]
-  (let [weights (map :weight opts)
-        deltas (map :delta opts)
-        vegas (map :vega opts)
-        length (count weights)]
-        (loop [counter 0 opt-delta 0]
-          (if (= counter (- length 1))
-          (* opt-delta 100)
-          (let [adj_delta (+ (nth deltas counter) (* (nth vegas counter) dvol-dK))]
-            (recur (inc counter) (+ opt-delta (* (nth weights counter) adj_delta))))))))
 
 
